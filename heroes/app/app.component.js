575cd16e290a3ef033842e437efa9df8ed2771d3
Alexandru Ionica <developer.lex@gmail.com>
2015-12-22 23:18:19 +0000

    structural directives

--- heroes/app/app.component.js
+++ heroes/app/app.component.js
@@ -9,7 +9,7 @@ System.register(['angular2/core'], function(exports_1) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
     var core_1;
-    var AppComponent;
+    var AppComponent, HEROES;
     return {
         setters:[
             function (core_1_1) {
@@ -20,21 +20,25 @@ System.register(['angular2/core'], function(exports_1) {
             AppComponent = (function () {
                 function AppComponent() {
                     this.title = 'Tour of Heroes';
-                    this.hero = {
-                        id: 1,
-                        name: 'Windstorm'
-                    };
+                    this.heroes = HEROES;
                 }
+                AppComponent.prototype.onSelect = function (hero) { this.selectedHero = hero; };
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: "<h1>{{title}}</h1>\n             <h2>{{hero.name}} details!</h2>\n             <div><label>id: </label>{{hero.id}}</div>\n             <div>\n              <label>name: </label>\n              <div><input [(ngModel)]=\"hero.name\" placeholder=\"name\"></div>"
+                        styles: ["\n    .heroes {list-style-type: none; margin-left: 1em; padding: 0; width: 10em;}\n    .heroes li { cursor: pointer; position: relative; left: 0; transition: all 0.2s ease; }\n    .heroes li:hover {color: #369; background-color: #EEE; left: .2em;}\n    .heroes .badge {\n      font-size: small;\n      color: white;\n      padding: 0.1em 0.7em;\n      background-color: #369;\n      line-height: 1em;\n      position: relative;\n      left: -1px;\n      top: -1px;\n    }\n    .selected { background-color: #EEE; color: #369; }\n  "],
+                        template: "<h1>{{title}}</h1>\n             <h2>My heroes</h2>\n             <ul class=\"heroes\">\n                <li *ngFor=\"#hero of heroes\"\n                    [class.selected]=\"hero === selectedHero\"\n                    (click)=\"onSelect(hero)\">\n                <li *ngFor=\"#hero of heroes\"\n        [class.selected]=\"hero === selectedHero\"\n        (click)=\"onSelect(hero)\">\n                 <span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n               </li>\n             </ul>\n             <div *ngIf=\"selectedHero\">\n               <h2>{{selectedHero.name}} details!</h2>\n               <div><label>id: </label>{{selectedHero.id}}</div>\n               <div>\n                  <label>name: </label>\n                  <div><input [(ngModel)]=\"selectedHero.name\" placeholder=\"name\"></div>\n               </div>\n             </div>"
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
                 return AppComponent;
             })();
             exports_1("AppComponent", AppComponent);
+            HEROES = [
+                { "id": 11, "name": "Mr. Nice" },
+                { "id": 12, "name": "Narco" },
+                { "id": 13, "name": "Bombasto" }
+            ];
         }
     }
 });
